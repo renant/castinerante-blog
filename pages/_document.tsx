578ea04e5921998df94105350475a89c91a96347
Renan Teixeira <renan.replay@live.com>
2020-06-14 13:29:29 -0300

    adjust favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,6 +28,123 @@ class MyDocument extends Document {
       sheet.seal();
     }
   }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="JYavb9_lp36yd2nl27ETTWRU63FozsNQ6eQaWpxvVdk"
+          />
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width,height=device-height"
+            key="viewport"
+          />
+          <title>Castinerante - Podcast Castinerante</title>
+          <meta name="description" content="Podcast Castinerante" />
+          <link rel="canonical" href="https://castinerante.com/" />
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:type" content="website" />
+          <link rel="shortcut icon" href="public/favicon.ico" />
+          <meta
+            property="og:title"
+            content="Castinerante - Podcast Castinerante"
+          />
+          <meta property="og:description" content="Podcast Castinerante" />
+          <meta property="og:url" content="https://castinerante.com/" />
+          <meta property="og:site_name" content="Castinerante" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:description" content="Podcast Castinerante" />
+          <meta
+            name="twitter:title"
+            content="Castinerante - Podcast Castinerante"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="msapplication-TileColor" content="#5e239d" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#5e239d" />
+        </Head>
+
+        <body>
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
