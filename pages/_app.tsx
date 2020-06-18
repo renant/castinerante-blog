85b7ae3bb1135cb2479d0d5ebd5b6835b202a28a
Renan Teixeira <renan.replay@live.com>
2020-06-17 22:52:11 -0300

    configure analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Router from 'next/router';
 import GlobalStyle from '../styles/global';
 import Header from '../components/Header';
 import SideMenu from '../components/SideMenu';
 import { Container, MainContainer } from '../styles/style';
+import * as gtag from '../lib/gtag.js';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    Router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return (
     <MainContainer>
       <GlobalStyle />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { GA_TRACKING_ID } from '../lib/gtag.js';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any): Promise<any> {
@@ -33,6 +34,25 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+
           <meta
             name="google-site-verification"
             content="JYavb9_lp36yd2nl27ETTWRU63FozsNQ6eQaWpxvVdk"
